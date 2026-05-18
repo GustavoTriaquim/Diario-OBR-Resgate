85722944533140d606bb4f59992e302e50f4ff22
Gustavo <gustavotriaquim6@gmail.com>
2026-05-18 20:04:01 -0300

    correcaoFirebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmUqhDkxIMm24u9apKfRuSmlbO14MGEtg",
-  authDomain: "robotica-2026-papa.firebaseapp.com",
-  projectId: "robotica-2026-papa",
-  storageBucket: "robotica-2026-papa.firebasestorage.app",
-  messagingSenderId: "1016569195298",
-  appId: "1:1016569195298:web:f0b84615ed6097e2e1537b",
-  measurementId: "G-6M4E56FQ5T"
+  apiKey: "AIzaSyDlDSlQoZUx2AVJu_cQLFcg_VA23uTuwe4",
+  authDomain: "diariosdebordo-2026.firebaseapp.com",
+  projectId: "diariosdebordo-2026",
+  storageBucket: "diariosdebordo-2026.firebasestorage.app",
+  messagingSenderId: "543151893278",
+  appId: "1:543151893278:web:26c5d7ddf891b2fdb0770f",
+  measurementId: "G-TWXZT2Q1TE"
 };
 
 const app = initializeApp(firebaseConfig);
